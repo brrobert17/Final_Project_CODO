4c2fcb6a032ed21207c3f05e94ee72f363d07e24
Adam0Schulz <adam@schulz.sk>
2023-11-08 11:48:41 +0100

    added basic router setup

--- MobileFrontEnd/App.tsx
+++ MobileFrontEnd/App.tsx
@@ -1,9 +1,28 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Text, View } from 'react-native';
+import Home from './screens/Home';
+
+export type StackParams = {
+  Home: {}
+}
+
+const Stack = createNativeStackNavigator<StackParams>();
 
 export default function App() {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName='Home'
+      >
+        <Stack.Screen
+          name='Home'
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
