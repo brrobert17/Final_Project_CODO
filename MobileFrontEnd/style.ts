e975885c3f737b43b4bf63708afcdbebbbc291fd
brrobert17 <br.robert17@gmail.com>
2023-11-08 11:19:10 +0100

    init expo helloWorld

---
+++ MobileFrontEnd/style.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from "react-native";
+const backGroundColor = '#000F1E'
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: backGroundColor,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    basicText: {
+        color: '#fff',
+    }
+});
