38385806d36113378fce1d00f4a8670f9e5574ed
Adam0Schulz <adam@schulz.sk>
2023-11-09 12:38:41 +0100

    added category section folder (commit only for the purpose of pull)

---
+++ MobileFrontEnd/components/CategorySection/CategorySection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import style from './style';
+
+export const CategorySection = () => {
+    return (
+        <View>
+            
+            
+        </View>
+    );
+};
