8c2560389bbd4a9ad425fc9621a511e600c40b15
Adam0Schulz <adam@schulz.sk>
2023-11-10 17:26:40 +0100

    fixed minor inconsistencies

--- MobileFrontEnd/components/Header/Header.tsx
+++ MobileFrontEnd/components/Header/Header.tsx
@@ -1,35 +1,33 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import { View, Text, Image, TouchableOpacity, SafeAreaView } from "react-native";
 import style from "./style";
-import gStyle from "@gStyle";
 import HeaderBtn from "./HeaderBtn/HeaderBtn";
 import SearchBar from "@components/SearchBar";
 import CategorySection from "@components/CategorySection";
-import {useNavigation} from "@react-navigation/native";
-import {NativeStackNavigationProp} from "@react-navigation/native-stack";
-import {StackParams} from "../../App";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParams } from "../../App";
 
 
 
 const Header = () => {
     const nav = useNavigation<NativeStackNavigationProp<StackParams>>();
     return (
-        <SafeAreaView style={style.headerCont}>
+        <View style={style.headerCont}>
             <SafeAreaView style={style.safeAreaCont}>
 
                 <Image style={style.logo} source={require("@assets/logo-schulz.png")}></Image>
                 <View style={style.btnCont}>
-                    <HeaderBtn.Cart onPress={() => console.log("action: go to cart")}/>
-                    <HeaderBtn.Burger onPress={() => nav.navigate('Menu', {})}/>
+                    <HeaderBtn.Cart onPress={() => console.log("action: go to cart")} />
+                    <HeaderBtn.Burger onPress={() => nav.navigate('Menu', {})} />
                 </View>
                 <SearchBar />
                 <CategorySection categories={[
-                                             {name: "corals", img: {url: "https://picsum.photos/201", alt: "something something"}},
-                                             {name: "fish", img: {url: "https://picsum.photos/202", alt: "something something"}},
-                                             {name: "other", img: {url: "https://picsum.photos/203", alt: "something something"}},
-                                             ]}/>
+                    { name: "corals", img: { url: "https://picsum.photos/201", alt: "something something" } },
+                    { name: "fish", img: { url: "https://picsum.photos/202", alt: "something something" } },
+                    { name: "other", img: { url: "https://picsum.photos/203", alt: "something something" } },
+                ]} />
             </SafeAreaView>
-        </SafeAreaView>
+        </View>
     )
 }
 
