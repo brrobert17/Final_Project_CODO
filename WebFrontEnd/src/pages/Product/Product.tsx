6372ab35463f90ef4511378aeb937e81c7cc0738
Adam0Schulz <adam@schulz.sk>
2023-11-23 16:43:52 +0100

    added the price tag component

--- WebFrontEnd/src/pages/Product/Product.tsx
+++ WebFrontEnd/src/pages/Product/Product.tsx
@@ -1,6 +1,9 @@
 import './style.css';
 import NavBar from "@components/NavBar";
 import {navigationPages} from "../Home/Home";
+import PriceTag from '@components/PriceTag';
+import ImageSlider from '@components/ImageSlider';
+
 export const Product = () => {
   return(
       <>
@@ -8,6 +11,8 @@ export const Product = () => {
           <div>
               Single Product
           </div>
+          <ImageSlider />
+          <PriceTag text='500' />
       </>
   )
 }
