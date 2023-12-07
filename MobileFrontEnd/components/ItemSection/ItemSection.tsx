3bfc4c19c4469b821f7adaaf8a73a0107da0f82a
Adam0Schulz <adam@schulz.sk>
2023-12-07 11:16:10 +0100

    fixed a minor bug in products with capitalizing heading

--- MobileFrontEnd/components/ItemSection/ItemSection.tsx
+++ MobileFrontEnd/components/ItemSection/ItemSection.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import {Image as IImage} from '@utils/interfaces'
+import { Image as IImage } from '@utils/interfaces'
 import style from './style';
 import gStyle from '@gStyle';
 import Dropdown from '@components/DropDown'
 import CategoryCard, { CategoryProps } from '@components/CategoryCard';
 import ProductCard, { ProductProps } from '@components/ProductCard';
-import {useNavigation} from "@react-navigation/native";
-import {NativeStackNavigationProp} from "@react-navigation/native-stack";
-import {StackParams} from "../../App";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParams } from "../../App";
 import { capitalizeWords } from '@utils/utils';
 
 interface Props {
@@ -16,10 +16,10 @@ interface Props {
     items: CategoryProps[] | ProductProps[],
     sorting?: boolean,
     seeMore?: {
-        func: ()=> void,
+        func: () => void,
         img: IImage
     }
-    
+
 }
 
 const ItemSection = (props: Props) => {
@@ -29,27 +29,27 @@ const ItemSection = (props: Props) => {
     return (
         <View style={isProduct ? style.contMargin : style.cont} >
             <View style={style.header}>
-                {props.seeMore 
-                ? 
+                {props.seeMore
+                    ?
                     <TouchableOpacity onPress={props.seeMore.func}>
                         <Text style={style.heading}>{capitalizeWords(props.heading)}</Text>
                     </TouchableOpacity>
-                :
-                <Text style={style.heading}>{props.heading}</Text>
+                    :
+                    <Text style={style.heading}>{capitalizeWords(props.heading)}</Text>
                 }
-                
+
                 {props.sorting ? <Dropdown onChange={(foo) => console.log(foo)} /> : <></>}
             </View>
             <View style={style.list}>
                 {props.items.map((item, index) => {
-                    if(!('price' in item)) {
-                        return <CategoryCard key={index} name={item.name} img={item.img} id={item.id}/>
+                    if (!('price' in item)) {
+                        return <CategoryCard key={index} name={item.name} img={item.img} id={item.id} />
                     } else {
                         return <ProductCard key={index} name={item.name} price={item.price} img={item.img} id={item.id} />
                     }
-                    
+
                 })}
-                {isProduct && props.seeMore ? <CategoryCard seeMoreVariant name={props.heading} func={props.seeMore.func} img={{url: props.seeMore.img.url, alt: props.seeMore.img.alt}}/> : <></>}
+                {isProduct && props.seeMore ? <CategoryCard seeMoreVariant name={props.heading} func={props.seeMore.func} img={{ url: props.seeMore.img.url, alt: props.seeMore.img.alt }} /> : <></>}
             </View>
         </View>
     );
