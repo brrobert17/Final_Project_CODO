8cef34989110fffc117f588a011c3ae2f003613d
Adam0Schulz <adam@schulz.sk>
2024-05-12 14:14:51 +0200

    added product page responsivity (more or less)

--- WebFrontEnd/src/components/LargeHeading/LargeHeading.tsx
+++ WebFrontEnd/src/components/LargeHeading/LargeHeading.tsx
@@ -2,15 +2,15 @@ import "./style.css";
 
 
 interface Props {
-    heading: string,
-    watermark: string
+  heading: string,
+  watermark: string,
 }
 
 const LargeHeading = (props: Props) => {
   return (
     <div className="large-heading">
-        <h1 className="large-heading__heading">{props.heading}</h1>
-        <h1 className="large-heading__watermark">{props.watermark}</h1>
+      <h1 className="large-heading__heading">{props.heading}</h1>
+      <h1 className="large-heading__watermark">{props.watermark}</h1>
     </div>
   )
 }
--- WebFrontEnd/src/pages/Product/Product.tsx
+++ WebFrontEnd/src/pages/Product/Product.tsx
@@ -1,19 +1,19 @@
 import './style.css';
 import NavBar from "@components/NavBar";
-import {navigationPages} from "../Home/Home";
+import { navigationPages } from "../Home/Home";
 import PriceTag from '@components/PriceTag';
 import ImageSlider from '@components/ImageSlider';
 import Button from '@components/Button';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import QuantitySelector from "@components/QuantitySelector";
-import {useLocation} from "react-router-dom";
-import {useProduct} from "@dbConn/hooks/UseProducts";
+import { useLocation } from "react-router-dom";
+import { useProduct } from "@dbConn/hooks/UseProducts";
 import Breadcrumbs from "@components/Breadcrumbs";
 import blob from "@assets/Blob.svg";
 import LargeHeading from '@components/LargeHeading';
 import ItemSection from '@components/ItemSection';
-import {useCategory} from '@dbConn/hooks/UseCategories';
-import {QueryParamsRelated} from "@interfaces";
+import { useCategory } from '@dbConn/hooks/UseCategories';
+import { QueryParamsRelated } from "@interfaces";
 
 export const RELATED_LIMIT = 5;
 
@@ -26,11 +26,11 @@ export const Product = () => {
     // States
     const [quantity, setQuantity] = useState(1);
     const [queryParamsRelated, setQueryParamsRelated] = useState<QueryParamsRelated>(
-        {productId: productId ? productId : '', exclude: true, limit: RELATED_LIMIT, type: 'related'}
+        { productId: productId ? productId : '', exclude: true, limit: RELATED_LIMIT, type: 'related' }
     );
 
     // React query hooks
-    const {isLoading: dataLoading, isError: dataError, data} = useProduct(productId as string);
+    const { isLoading: dataLoading, isError: dataError, data } = useProduct(productId as string);
     const {
         isLoading: categoryLoading,
         isError: categoryError,
@@ -40,32 +40,40 @@ export const Product = () => {
     // use Effects
     useEffect(() => {
         window.scrollTo(0, 0);
-        setQueryParamsRelated({productId: productId ? productId : '', exclude: true, limit: RELATED_LIMIT, type: 'related'})
+        setQueryParamsRelated({ productId: productId ? productId : '', exclude: true, limit: RELATED_LIMIT, type: 'related' })
     }, [location]);
 
     return (
         <div className='page'>
-            <NavBar pages={navigationPages} loginUrl={'/login'} shoppingCartUrl={'/cart'}/>
+            <NavBar pages={navigationPages} loginUrl={'/login'} shoppingCartUrl={'/cart'} />
             <div className='product__main'>
                 <div className='product__breadcrumbs'>
-                    {data?.category && <Breadcrumbs categoryId={data.category}/>}
+                    {data?.category && <Breadcrumbs categoryId={data.category} />}
                 </div>
-                <div className='product__left'>
-                    <img className='product__left__blob' src={blob} alt='Background decoration blob'></img>
-                    {data && <ImageSlider images={data.img}/>}
+                <div className='product__show'>
+                    <img className='product__show__blob' src={blob} alt='Background decoration blob'></img>
+                    {data && <ImageSlider images={data.img} />}
                 </div>
-                <div className='product__right'>
-                    {data && <LargeHeading heading={data.name} watermark={categoryData ? categoryData.name : ''}/>}
+
+                <div className='product__heading'>
+                    {data && <LargeHeading heading={data.name} watermark={categoryData ? categoryData.name : ''} />}
+                </div>
+                <div className='product__description'>
                     <p>{data && data.description}</p>
-                    <PriceTag text={data ? Number(data.price) : 0}/>
-                    <QuantitySelector quantity={quantity} onChange={setQuantity}/>
-                    <div className='product__btn-cont'>
-                        <Button text='Add to cart' onClick={() => console.log("hello world")}/>
-                        <Button secondary text='Buy now' onClick={() => console.log("hello world")}/>
-                    </div>
                 </div>
+                <div className='product__pricetag'>
+                    <PriceTag text={data ? Number(data.price) : 0} />
+                </div>
+                <div className='product__quantity-select'>
+                    <QuantitySelector quantity={quantity} onChange={setQuantity} />
+                </div>
+                <div className='product__btn-cont'>
+                    <Button text='Add to cart' onClick={() => console.log("hello world")} />
+                    <Button secondary text='Buy now' onClick={() => console.log("hello world")} />
+                </div>
+
                 <div className='product__related'>
-                    <ItemSection heading={"Related Products"} itemType={'Product'} queryParams={queryParamsRelated}/>
+                    <ItemSection heading={"Related Products"} itemType={'Product'} queryParams={queryParamsRelated} />
                 </div>
             </div>
         </div>
